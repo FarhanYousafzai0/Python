46598e9d7169bf35d7cfd049291bc7f9352874a5
Farhan Yousafzai <coderwithferry@gmail.com>
2025-04-29 00:56:12 +0500

    lesson 4

--- app.py
+++ app.py
@@ -1,84 +1,137 @@
-# ================== Taking Input and Finding the Greatest Number ==================
+# # ================== Taking Input and Finding the Greatest Number ==================
 
-firstNumber = int(input("Enter first number: "))
-secondNumber = int(input("Enter second number: "))
-thirdNumber = int(input("Enter third number: "))
+# firstNumber = int(input("Enter first number: "))
+# secondNumber = int(input("Enter second number: "))
+# thirdNumber = int(input("Enter third number: "))
 
-if firstNumber > secondNumber and firstNumber > thirdNumber:
-    print("First number is greatest")
-elif secondNumber > firstNumber and secondNumber > thirdNumber:
-    print("Second number is greatest")
-elif thirdNumber > firstNumber and thirdNumber > secondNumber:
-    print("Third number is greatest")
-else:
-    print("All numbers are equal")
+# if firstNumber > secondNumber and firstNumber > thirdNumber:
+#     print("First number is greatest")
+# elif secondNumber > firstNumber and secondNumber > thirdNumber:
+#     print("Second number is greatest")
+# elif thirdNumber > firstNumber and thirdNumber > secondNumber:
+#     print("Third number is greatest")
+# else:
+#     print("All numbers are equal")
 
-# ================== Check if a Number is Divisible by 7 ==================
+# # ================== Check if a Number is Divisible by 7 ==================
 
-number = int(input("Enter a number: "))
-if number % 7 == 0:
-    print(f"The {number} is divisible by 7")
-else:
-    print(f"The {number} is not divisible by 7")
+# number = int(input("Enter a number: "))
+# if number % 7 == 0:
+#     print(f"The {number} is divisible by 7")
+# else:
+#     print(f"The {number} is not divisible by 7")
 
-# ================== Unpacking a Tuple ==================
+# # ================== Unpacking a Tuple ==================
 
-fruits = ["apple", "banana", "cherry"]
-x, y, z = fruits
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
 
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
 
-# ================== Create a Student List ==================
+# # ================== Create a Student List ==================
 
-student = ["Farhan", 44, "Pakistan", 44.3]
+# student = ["Farhan", 44, "Pakistan", 44.3]
 
-# ================== Append a Number to List ==================
+# # ================== Append a Number to List ==================
 
-list1 = [1, 2, 3, 4]
-list1.append(6)
-print(list1)
+# list1 = [1, 2, 3, 4]
+# list1.append(6)
+# print(list1)
 
-# ================== Find Index of a Number in Tuple ==================
+# # ================== Find Index of a Number in Tuple ==================
 
-tup = (1, 11, 34, 1, 22, 2, 2)
-print(tup.index(2))
+# tup = (1, 11, 34, 1, 22, 2, 2)
+# print(tup.index(2))
 
-# ================== Adding Movies to a List ==================
+# # ================== Adding Movies to a List ==================
 
-Movie1 = input("Enter first movie name: ")
-Movie2 = input("Enter second movie name: ")
-Movie3 = input("Enter third movie name: ")
+# Movie1 = input("Enter first movie name: ")
+# Movie2 = input("Enter second movie name: ")
+# Movie3 = input("Enter third movie name: ")
 
-movieList = []
-movieList.append(Movie1)
-movieList.append(Movie2)
-movieList.append(Movie3)
+# movieList = []
+# movieList.append(Movie1)
+# movieList.append(Movie2)
+# movieList.append(Movie3)
 
-print("Movie List:", movieList)
+# print("Movie List:", movieList)
 
-# ================== Check if a List is a Palindrome ==================
+# # ================== Check if a List is a Palindrome ==================
 
-list2 = [1, 2, 2, 1]
+# list2 = [1, 2, 2, 1]
 
-copy_list = list2.copy()
-copy_list.reverse()
+# copy_list = list2.copy()
+# copy_list.reverse()
 
-if copy_list == list2:
-    print("Palindrome")
-else:
-    print("Not Palindrome")
+# if copy_list == list2:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
 
-# ================== Count Number of Students Starting with 'A' ==================
+# # ================== Count Number of Students Starting with 'A' ==================
 
-grades = ('B', 'B', 'C', 'A', 'A', 'A', 'D', 'D', 'D', 'F')
+# grades = ('B', 'B', 'C', 'A', 'A', 'A', 'D', 'D', 'D', 'F')
 
-print("Count of 'A':", grades.count('A'))
-print("Count of 'B':", grades.count('B'))
+# print("Count of 'A':", grades.count('A'))
+# print("Count of 'B':", grades.count('B'))
 
-# ================== Store Tuple in List and Sort ==================
+# # ================== Store Tuple in List and Sort ==================
 
-grades_list = list(grades)
-grades_list.sort()
-print("Sorted Grades:", grades_list)
+# grades_list = list(grades)
+# grades_list.sort()
+# print("Sorted Grades:", grades_list)
+
+
+
+#  Dictionary & Set in Python
+
+# Dictoniary are used to store data values in key:value paris.They are unordered and mutable and dont allow duplicate key:
+
+
+# Simple Disctoniary:with all data types 
+
+# dict = {
+
+# "name" : "Farhan Yousafzau",
+# "roll_no" : 4404,
+# "subject" :['Computer_Science','English'],
+# "Awards" : ('Full Stack Developer',"Generative Ai master","CEO of techNino"),
+# "Batch_no": 17
+# }
+
+# print(dict.items())
+# # Nested Distionary:
+
+
+# nest_Dict = {
+#  " school_name" : 'Gurus Tech Hub',
+#  "branches" : {
+#      "nowshera" :"Frontend",
+#      "islamabad" : "Backend",
+#      "Lahore" : "Devops"
+#  }
+
+# }
+
+
+
+
+
+
+#Dictoniary Methods
+# .keys() --> return all the keys but not the nested key
+# .values() -----> return all the values of keys 
+# .items() --->   return all the (key,values) as tuple -->();
+# .get() ---> return the the key according to the values
+# .update() -->insert the specified items in the dict.                                        #
+
+
+
+
+#SET IN PYTHON: DF
+# SET IN THE COLLECTIONS OF UNORDERED ITEMS,EACH ELEMENT IN THE SET MUST BE UNIQUE AND MUTABLE.
+# 
+#  
+
