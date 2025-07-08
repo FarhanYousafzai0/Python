d880a680d3929f2afed2b6f378089140bd1ec7c2
Farhan Yousafzai <coderwithferry@gmail.com>
2025-07-08 20:18:02 +0500

    4 hours

--- Python_Day_1.py
+++ Python_Day_1.py
@@ -1,3 +1,6 @@
+
+#---------------Numbers-----------#
+
 # character_name = "Farhan"
 # character_age = "20"
 
@@ -41,23 +44,78 @@
 
 
 
+
+# Numbers----------------
+
 # Bulding Basic Calculator:
 
 
-a = float(input("Enter the first number :"))
-b = float(input("Enter the second number :"))
+# a = float(input("Enter the first number :"))
+# b = float(input("Enter the second number :"))
 
 
-print("Sum",a + b);
-print('Differnce', a -b);
-print("Product",a *b);
-print("Division", a / b);
+# print("Sum",a + b);
+# print('Differnce', a -b);
+# print("Product",a *b);
+# print("Division", a / b);
 
-# Finding the Area of cirlce:
-radius = float(input("Enter the radius if circle:"));
-
-
-area = 3.1416 *(radius ** 2);
+# # Finding the Area of cirlce:
+# radius = float(input("Enter the radius if circle:"));
 
 
-print("The radius of circle is :",area)
+# area = 3.1416 *(radius ** 2);
+
+
+# print("The radius of circle is :",area)
+
+
+
+# Creating Mad Labs Game:
+
+
+# color = input("Enter your Color : ")
+
+# plural_noun = input("Enter you Plural_Noun : ")
+
+# celebratiy = input("Enter Celebrity Name :")
+
+# print("\n-- Your Poem --")
+
+# print(f"Roses are {color}")
+# print(f"{plural_noun} are blue.")
+# print(f"I love {celebratiy}")
+
+# print("\n-- Your Poem --")
+
+
+
+
+
+
+
+# ----------------------Lists----------------#
+
+
+friends = ["Farhan","Musa","Hamza","ALi","Haseeb","Me"]
+Numbers = [2,3,7,5,4,3,6,1,]
+
+
+Numbers.pop()
+
+print(Numbers)
+
+
+# Create a Favorite Movies List
+
+
+Favoite_Anime = ["Attack on Titans","Naruto","Bleach","Jutsu Kaisan"]
+
+Favoite_Anime.append("Full Metal Alchemist");
+
+Favoite_Anime.insert(2,"Demon Slayers");
+
+
+Favoite_Anime.remove("Bleach");
+
+
+print(Favoite_Anime)
